5c47ca2905e04e62c93a34a98947e37d70aca70b
심승용 <seungyong.shim@mirero.co.kr>
2020-02-10 11:54:54 +0900

    코드 추가

--- ResourceMonitorAnalyzer.Console/Program.cs
+++ ResourceMonitorAnalyzer.Console/Program.cs
@@ -11,13 +11,15 @@ namespace ResourceMonitorAnalyzer.ConsoleApp
 {
     public class AnalyzedResult: Record<AnalyzedResult>
     {
+        public readonly string MachineName;
         public readonly string Header;
         public readonly decimal Average90;
         public readonly decimal Average90H10;
         public readonly decimal Average90L10;
 
-        public AnalyzedResult(string header, decimal average90, decimal average90H10, decimal average90L10)
+        public AnalyzedResult(string machineName, string header, decimal average90, decimal average90H10, decimal average90L10)
         {
+            MachineName = machineName;
             Header = header;
             Average90 = average90;
             Average90H10 = average90H10;
@@ -28,9 +30,9 @@ namespace ResourceMonitorAnalyzer.ConsoleApp
         {
             return new StringBuilder()
                 .AppendLine(Header)
-                .AppendLine($"  Average90: {Average90}")
-                .AppendLine($"  Average90H10: {Average90H10}")
-                .AppendLine($"  Average90L10: {Average90L10}")
+                .AppendLine($"  Avg90: {Average90}")
+                .AppendLine($"  Avg90H10: {Average90H10}")
+                .AppendLine($"  Avg90L10: {Average90L10}")
                 .ToString();
         }
     }
@@ -42,32 +44,33 @@ namespace ResourceMonitorAnalyzer.ConsoleApp
             using (var csv = File.Open("data.csv", FileMode.Open))
             using (var sr = new StreamReader(csv))
             {
-                var headers = ModifiedHeader(sr.ReadLine()).ToSeq(); // header
+                var rawHeader = sr.ReadLine();
+                var headers = ModifiedHeader(rawHeader).ToSeq(); // header
+                var machineName = GetMachineName(rawHeader);
 
                 var rows = new List<Seq<decimal>>();
 
                 while (sr.EndOfStream != true)
                 {
-                    var value = sr.ReadLine();
-                    var result = value.Split(',')
-                        .Select(x => string.Concat(x.AsEnumerable().Where(y => y != '\"')))
+                    var value = sr.ReadLine().Split(',');
+                    var dateTime = DateTime.Parse(string.Concat(value?[0].Filter(y => y != '\"')));
+                    var result = value.DefaultIfEmpty("")
+                        .Select(x => string.Concat(x.AsEnumerable().Filter(y => y != '\"')))
                         .Select(x => parseDecimal(x)
-                            .Match(_ => _, () => -1))
+                            .Match(_ => _, () => decimal.MinValue))
                         .ToSeq();
 
                     rows.Add(result);
                 }
 
-                var totalCount = rows.Count;
-
                 for (int i = 1; i < headers.Count; i++)
                 {
                     var columns = rows.Map(x => x.Skip(i).FirstOrDefault()).ToSeq();
-                    var ordered = columns.OrderBy(x => x).ToSeq();
-                    var low05 = ordered.Skip(Convert.ToInt32(totalCount * 0.05)).FirstOrDefault();
-                    var low14 = ordered.Skip(Convert.ToInt32(totalCount * 0.14)).FirstOrDefault();
-                    var high05 = ordered.Skip(Convert.ToInt32(totalCount * (1 - 0.05))).FirstOrDefault();
-                    var high14 = ordered.Skip(Convert.ToInt32(totalCount * (1 - 0.14))).FirstOrDefault();
+                    var ordered = columns.Filter(x => x != decimal.MinValue).OrderBy(x => x).ToSeq();
+                    var low05 = ordered.Skip(Convert.ToInt32(ordered.Count * 0.05)).FirstOrDefault();
+                    var low14 = ordered.Skip(Convert.ToInt32(ordered.Count * 0.14)).FirstOrDefault();
+                    var high05 = ordered.Skip(Convert.ToInt32(ordered.Count * (1 - 0.05))).FirstOrDefault();
+                    var high14 = ordered.Skip(Convert.ToInt32(ordered.Count * (1 - 0.14))).FirstOrDefault();
 
                     // 상위 5%, 하위 5%를 제외한 평균
                     var average90 = columns.Filter(x => x > low05 && x < high05).DefaultIfEmpty().Average();
@@ -78,6 +81,7 @@ namespace ResourceMonitorAnalyzer.ConsoleApp
 
                     var result = new AnalyzedResult
                     (
+                        machineName,
                         headers[i],
                         average90,
                         average90H10,
--- ResourceMonitorAnalyzer/ParserCsv.cs
+++ ResourceMonitorAnalyzer/ParserCsv.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
+using static LanguageExt.Prelude;
 
 namespace ResourceMonitorAnalyzer
 {
@@ -14,6 +15,23 @@ namespace ResourceMonitorAnalyzer
                 .Select(x => x.Trim())
                 .Select(x => Regex.Replace(x, @"^[""][\\][\\]([^\\]*)[\\]", @""""))
                 .Select(x => x.Replace('[', '(').Replace(']', ')').Replace('\\','.'));
+        }
+
+        public static string GetMachineName(string source)
+        {
+            return string.Concat(
+                Regex.Match(
+                        source.Split(',')
+                            .Skip(1)
+                            .DefaultIfEmpty("")
+                            .FirstOrDefault()
+                            .Trim()
+                        , @"^[""][\\][\\]([^\\]*)[\\]")
+                    .Value
+                    .Filter(x => x != '\\')
+                    .Filter(x => x != '\"')
+            );
+
         }
     }
 }
